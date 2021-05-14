674ab760706afda9b681085edd6310b95defb907
Tung Pham <tung.42@gmail.com>
2021-05-14 12:19:42 +0700

    Update ai.js

--- imports/ui/pages/ai/ai.js
+++ imports/ui/pages/ai/ai.js
@@ -1,1 +1,21 @@
+import { Template } from 'meteor/templating';
+
 import './ai.html';
+
+Template.aiAbove.events({
+  'click .go-to-href'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const href = target["data-href"];
+    window.location.href = href;
+  }
+});
+
+Template.aiBelow.events({
+  'click .go-to-href'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const href = target["data-href"];
+    window.location.href = href;
+  }
+});
